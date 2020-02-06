e4003760791a850d6118586f9391ecad3615c900
bit_jonghyun <goste390@gmail.com>
2020-02-06 10:35:15 +0900

    add DB SETTING

--- AirFrash/AirFrashProject/src/main/mainclass.java
+++ AirFrash/AirFrashProject/src/main/mainclass.java
@@ -1,7 +1,7 @@
 package main;
 
 public class mainclass {
-public static void main(String[] args) {
-	System.out.println("hello world");
-}
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
 }
