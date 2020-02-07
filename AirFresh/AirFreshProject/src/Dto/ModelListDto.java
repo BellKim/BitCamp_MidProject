7d8814547afd6385c84bb24551adbbce4dc1e152
jane <pro.janecho@gmail.com>
2020-02-07 10:27:46 +0900

    Model List DTO

---
+++ AirFresh/AirFreshProject/src/Dto/ModelListDto.java
@@ -0,0 +1,75 @@
+package Dto;
+
+import java.io.Serializable;
+
+public class ModelListDto implements Serializable {
+
+	private int prd_index;					//번호
+	private String prd_name;				//제품명
+	private String prd_model_name;			//모델명
+	private String prd_price;				//가격
+
+	
+	public ModelListDto() {
+	
+	}
+
+
+	public ModelListDto(int prd_index, String prd_name, String prd_model_name, String prd_price) {
+		super();
+		this.prd_index = prd_index;
+		this.prd_name = prd_name;
+		this.prd_model_name = prd_model_name;
+		this.prd_price = prd_price;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ModelListDto [prd_index=" + prd_index + ", prd_name=" + prd_name + ", prd_model_name=" + prd_model_name
+				+ ", prd_price=" + prd_price + "]";
+	}
+
+
+	public int getPrd_index() {
+		return prd_index;
+	}
+
+
+	public void setPrd_index(int prd_index) {
+		this.prd_index = prd_index;
+	}
+
+
+	public String getPrd_name() {
+		return prd_name;
+	}
+
+
+	public void setPrd_name(String prd_name) {
+		this.prd_name = prd_name;
+	}
+
+
+	public String getPrd_model_name() {
+		return prd_model_name;
+	}
+
+
+	public void setPrd_model_name(String prd_model_name) {
+		this.prd_model_name = prd_model_name;
+	}
+
+
+	public String getPrd_price() {
+		return prd_price;
+	}
+
+
+	public void setPrd_price(String prd_price) {
+		this.prd_price = prd_price;
+	}
+
+
+	
+}
