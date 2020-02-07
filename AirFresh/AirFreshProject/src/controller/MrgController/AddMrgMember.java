3ec345b4f9166fd9facfa89aa79e646c00b88bf7
bit_jonghyun <goste390@gmail.com>
2020-02-07 17:04:41 +0900

    Revert "connect servlet"
    This reverts commit 72c8571fb3567f260f98639669c0bc321c4fec4b.

--- AirFresh/AirFreshProject/src/controller/MrgController/AddMrgMember.java
+++ AirFresh/AirFreshProject/src/controller/MrgController/AddMrgMember.java
@@ -1,31 +1,7 @@
 package controller.MrgController;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-
-
-//메니저 추가를 하기 위한 컨트롤러.
-@WebServlet("/AddMgrMember")
 public class AddMrgMember extends HttpServlet {
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("hello AddMrgMember do get ");
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("hello AddMrgMember doPost do get ");
-	}
-
-	
-	
-	
-	
-}//end AddMrgMember class
+}
