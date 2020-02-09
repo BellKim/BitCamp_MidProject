6c006610baaa3a28af91f995ff65d539a0ed5c8e
Bellkim <goste390@gmail.com>
2020-02-09 16:39:55 +0900

    modify files

--- AirFresh/AirFreshProject/WebContent/admin_view/manageMgr/showManagerAll.jsp
+++ AirFresh/AirFreshProject/WebContent/admin_view/manageMgr/showManagerAll.jsp
@@ -9,11 +9,18 @@
 <%-- 메니저 전체 리스트를 열람, 추가/삭제 해줌. --%>
 <body>
 	<h3> input data </h3>
+	<%=request.getContextPath() %>
+	<%--<%=request.getContextPath()%>/addMrgMember --%>
+	<form id="addManagerMem" name="addManagerMem" action="<%=request.getContextPath()%>/addMrgMember"  method="POST">
+		<input type="textarea" id="test" name="test" >
+		<input type="textarea" id="test1" name="test1" >
+	<input type="submit"  value="전송버튼">
 	
-	<form id="addManagerMem" name="addManagerMem" action="../../AddMgrMember"  method="post">
-		<input type="textarea" id="test" name="_test" >
+	<!-- <form id="addManagerMem" name="addManagerMem" action="./../../AddMgrMember"  method="post"> -->
 	
-		<input type="submit"  value="전송버튼">
+	
+	
+		
 		
 	</form>
 	
--- AirFresh/AirFreshProject/src/controller/MrgController/AddMrgMember.java
+++ AirFresh/AirFreshProject/src/controller/MrgController/AddMrgMember.java
@@ -10,13 +10,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/AddMgrMember")
+@WebServlet("/addMrgMember")
+			  
 public class AddMrgMember extends HttpServlet {
 	
-	private static AddMrgMember addMrgMember = null;
-	
-	private AddMrgMember() {
-	}
+//	private static AddMrgMember addMrgMember = null;
+//	
+//	private AddMrgMember() {
+//	}
 //	public AddMrgMember getInstance() {
 //		if(addMrgMember == null) {
 //			addMrgMember = new AddMrgMember();
@@ -28,23 +29,34 @@ public class AddMrgMember extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("hello AddMrgMember do get ");
+		
+		String input = req.getParameter("test");
+		String input1 = req.getParameter("test1");
+		
+		System.out.println("test1 = " + input + "\n test2 = " + input1);
+		
+		
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("hello AddMrgMember doPost do get ");
+		String input = req.getParameter("test");
+		String input1 = req.getParameter("test1");
+		
+		System.out.println("test1 = " + input + "\n test2 = " + input1);
 	}
 
-	protected void processing(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-	}
-	
-	protected void forward(String url, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//사용자가 보내온 요청에 의한 데이터를 가지고 와서 
-		//사용자가 요청한 데이터를 가지고 해당 View로 이동하는 함수 
-		RequestDispatcher dispatch = req.getRequestDispatcher(url);
-		dispatch.forward(req, resp);	
-	}
+//	protected void processing(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		
+//	}
+//	
+//	protected void forward(String url, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		//사용자가 보내온 요청에 의한 데이터를 가지고 와서 
+//		//사용자가 요청한 데이터를 가지고 해당 View로 이동하는 함수 
+//		RequestDispatcher dispatch = req.getRequestDispatcher(url);
+//		dispatch.forward(req, resp);	
+//	}
 	
 	
 	
