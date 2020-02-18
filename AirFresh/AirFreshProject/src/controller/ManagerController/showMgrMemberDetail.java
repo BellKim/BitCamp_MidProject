bfb2903ca130152a44976c34cbdfd3103ce15fec
bit_jonghyun <goste390@gmail.com>
2020-02-18 13:01:52 +0900

    add import list

--- AirFresh/AirFreshProject/src/controller/ManagerController/showMgrMemberDetail.java
+++ AirFresh/AirFreshProject/src/controller/ManagerController/showMgrMemberDetail.java
@@ -3,6 +3,7 @@ package controller.ManagerController;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
